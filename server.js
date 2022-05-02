bd9e1a3a8931da577f1c8ab521da9aa7c6991487
Michael J. Kozubal Jr <mjkjr84@gmail.com>
2022-05-02 11:07:09 -0400

    added timestamp endpoints

--- server.js
+++ server.js
@@ -8,22 +8,37 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// some legacy browsers choke on 204
+app.use(cors({optionsSuccessStatus: 200}));
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use( express.static( __dirname + '/public' ) );
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+	res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// date string timestamp api endpoint
+app.get("/api/:year-:month-:day", function (req, res) {
+
+	// convert timestamp date string into a date object
+	const date = new Date( req.params.year + '-' + req.params.month + '-' + req.params.day );
+
+	// return a json object with the date info
+	res.json( { unix: date.getTime(), utc: date.toUTCString() } );
 });
 
+// unix epoch timestamp api endpoint
+app.get("/api/:epoch", function (req, res) {
+
+	// convert timestamp date string into a date object
+	const date = new Date( Number( req.params.epoch ) );
+
+	// return a json object with the date info
+	res.json( { unix: date.getTime(), utc: date.toUTCString() } );
+});
 
 
 // listen for requests :)
